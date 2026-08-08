7e103a62b47c0aa8534c851d251c9c65e10b31dc
Sean Campbell <sean.campbell3161@gmail.com>
2026-08-07 23:00:27 -0500

    Scaffold National Parks Explorer frontend shell

---
+++ 01-national-parks-explorer/web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>National Parks Explorer</h1>
+      <p>Build me! See the app's README.md for the assignment.</p>
+    </main>
+  );
+}
+
+export default App;
